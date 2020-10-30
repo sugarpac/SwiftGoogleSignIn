24ed5714e0d70c5c77b1ceea02337b5a289caf41
Taisei Sato@MBP2018 <cotterets@gmail.com>
2020-10-30 12:10:54 +0900

    init

--- Package.swift
+++ Package.swift
@@ -4,7 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoogleSignIn",
+    name: "SwiftGoogleSignIn",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,16 +19,38 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "GTMSessionFetcher",
+            url: "https://github.com/google/gtm-session-fetcher.git",
+            "1.4.0" ..< "2.0.0"
+        ),
+        .package(
+            name: "GTMAppAuth",
+            url: "https://github.com/google/GTMAppAuth.git",
+            "1.0.0" ..< "2.0.0"
+        ),
+        .package(
+            name: "AppAuth",
+            url: "https://github.com/openid/AppAuth-iOS.git",
+            "1.4.0" ..< "2.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GoogleSignIn",
-            dependencies: []),
+            dependencies: [
+                .product(name: "GTMSessionFetcherCore", package: "GTMSessionFetcher"),
+                .product(name: "AppAuthCore", package: "AppAuth"),
+                .target(name: "GoogleSignInBinary", condition: .when(platforms: .some([.iOS]))),
+            ]),
         .testTarget(
             name: "GoogleSignInTests",
             dependencies: ["GoogleSignIn"]),
+        .binaryTarget(
+            name: "GoogleSignInBinary",
+            path: "artifacts/GoogleSignIn.xcframework"
+        )
     ]
 )
